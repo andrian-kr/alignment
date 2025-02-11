42479ace3248bffdf67add4415a10666b5ec0861
Andrian Kravchenko <akravchenko@gloriumtech.com>
2025-02-11 15:20:07 +0200

    Add ETHICS commonsense translated dataset

--- evaluation/setup_langfuse.py
+++ evaluation/setup_langfuse.py
@@ -1,9 +1,9 @@
 import os
 
+import pandas as pd
 from dotenv import load_dotenv
 from langfuse import Langfuse
 from tqdm import tqdm
-import pandas as pd
 
 load_dotenv()
 
@@ -14,6 +14,8 @@ def create_dataset(
     dataset_name: str,
     description: str,
     data: pd.DataFrame,
+    input_mapping: dict[str, str],
+    output_mapping: dict[str, str],
 ):
     print(f"Creating dataset {dataset_name}...")
 
@@ -25,24 +27,37 @@ def create_dataset(
     for _, row in tqdm(data.iterrows(), total=len(data)):
         langfuse_client.create_dataset_item(
             dataset_name=dataset.name,
-            input={"query": row["inputs"]},
-            expected_output={"targets": row["targets"]},
+            input={key: row[source_key] for key, source_key in input_mapping.items()},
+            expected_output={key: row[source_key] for key, source_key in output_mapping.items()},
         )
     print(f"Dataset {dataset_name} created.")
 
 
-def create_all_datasets(dataset_dir: str = "data"):
-    for file in os.listdir(dataset_dir):
-        if file.endswith(".csv"):
-            dataset_name = file.split(".")[0]
-            dataset_path = os.path.join(dataset_dir, file)
-            data = pd.read_csv(dataset_path)
+def create_ethics_dataset(file_path: str = "data/ethics_commonsense.csv"):
+    dataset_name = os.path.basename(file_path).split(".")[0]
+    data = pd.read_csv(file_path)
 
-            create_dataset(
-                dataset_name,
-                f"Test inputs and target outputs for {dataset_name}.",
-                data,
-            )
+    create_dataset(
+        dataset_name,
+        f"Test inputs and target outputs for {dataset_name}.",
+        data,
+        input_mapping={"input": "input_ukr", "input_en": "input"},
+        output_mapping={"label": "label"},
+    )
 
 
-create_all_datasets()
+create_ethics_dataset()
+
+
+# def create_all_datasets(dataset_dir: str = "data"):
+#     for file in os.listdir(dataset_dir):
+#         if file.endswith(".csv"):
+#             dataset_name = file.split(".")[0]
+#             dataset_path = os.path.join(dataset_dir, file)
+#             data = pd.read_csv(dataset_path)
+
+#             create_dataset(
+#                 dataset_name,
+#                 f"Test inputs and target outputs for {dataset_name}.",
+#                 data,
+#             )
